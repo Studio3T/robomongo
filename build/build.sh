86d71e8b6ab3e76195e95abcb36b362f95f4abc1
schetnikovich <tschetnikovich@gmail.com>
2012-11-09 18:24:36 +0300

    build script support for release and debug modes

--- build/build.sh
+++ build/build.sh
@@ -2,16 +2,36 @@
 #  Configuration
 # ****************
 
-PROJECT_ROOT=`pwd`/.. # Find project root
-ORIGINAL_DIR=`pwd`    # Store current directory in order to return to it after
-TARGET=${PROJECT_ROOT}/target/debug # Prepare output directory name
-OUTPUT=${TARGET}/out # Prepare output directory name
+MODE=$1
+
+if [ -z $MODE ]; then
+   MODE=debug
+fi
+
+if [[ $MODE != "release" ]] && [[ $MODE != "debug" ]]; then
+    echo
+    echo -e "\e[00;31m Specified mode ($MODE) is unsupported.\e[00m"
+    echo
+    exit 1
+fi
+
+PROJECT_ROOT=`pwd`/..                  # Find project root
+ORIGINAL_DIR=`pwd`                     # Store current directory in order to return to it after
+TARGET=${PROJECT_ROOT}/target/$MODE    # Prepare output directory name
+OUTPUT=${TARGET}/out                   # Prepare output directory name
+
+if [[ $MODE = "debug" ]]; then
+  QMAKE_ARGS="-r -spec linux-g++ CONFIG+=debug CONFIG+=declarative_debug"
+fi
+if [[ $MODE = "release" ]]; then
+  QMAKE_ARGS="-r -spec linux-g++ CONFIG+=release"
+fi
 
 # ****************
 # Preparation
 # ****************
 
-# Create target folder
+# Create target folder (if not already exists)
 mkdir -p $TARGET
 echo Output folder $TARGET
 
@@ -22,7 +42,7 @@ cd $TARGET
 # qmake
 # ****************
 
-qmake $PROJECT_ROOT/src/robomongo.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=declarative_debug
+qmake $PROJECT_ROOT/src/robomongo.pro $QMAKE_ARGS
 
 if [ $? != 0 ]; then
 {

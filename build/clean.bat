2406caaf53ac1392cde4d293a15d9e85b89aadbc
Dmitry Schetnikovich <tschetnikovich@gmail.com>
2012-11-09 19:49:59 +0300

    support for debug and release in build script. clean script added

---
+++ build/clean.bat
@@ -0,0 +1,58 @@
+@echo off 
+setlocal enableextensions enabledelayedexpansion
+
+rem -----------------------------------
+rem - Configuration
+rem -----------------------------------
+
+rem store current directory in order to return to it after
+set ORIGINAL_DIR=%CD%
+
+rem find project root folder
+set PROJECT_ROOT=%CD%\..\
+
+rem mode is a first arg
+set MODE=%1
+
+rem if mode wasnt specified - debug mode assumed
+if %1. ==. (
+  set MODE=debug
+)
+
+rem if not 'debug' AND 'release' - print error message
+if not %MODE% == debug (
+  if not %MODE% == release (
+    echo.
+    echo Specified mode ^(%MODE%^) is unsupported.
+    exit /b 1
+    pause
+  )
+)
+
+rem target folder where build will be placed
+set TARGET=%PROJECT_ROOT%target\%MODE%
+
+if not exist %TARGET% (
+  echo.
+  echo Already clean: !TARGET!.
+  exit /b 1
+  pause
+)
+
+rem remove target folder
+rmdir /s /q %TARGET%
+if %ERRORLEVEL% neq 0 (
+  echo.
+  echo Error when removing !TARGET!.
+  exit /b 1
+  pause
+)
+
+if not exist %TARGET% (
+  echo.
+  echo Done without errors.
+  echo This folder removed: !TARGET!
+) else (
+  echo.
+  echo Error when removing files. Not all files were removed.
+)

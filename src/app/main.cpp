9a0b6517d60dcb33dba4d6a210e779d4d3989dad
schetnikovich <tschetnikovich@gmail.com>
2012-11-16 20:39:52 +0300

    run.sh for linux

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,6 +4,7 @@
 #include <QMessageBox>
 #include <QList>
 #include <QSharedPointer>
+#include <QProcess>
 //#include <QDir>
 //#include "mainwindow.h"
 //#include "boost/shared_ptr.hpp"
@@ -19,70 +20,29 @@
 
 using namespace Robomongo;
 
-class Base {};
-class Deli : public Base {};
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QProcessEnvironment proc = QProcessEnvironment::systemEnvironment();
+    QString str = proc.value("LD_LIBRARY_PATH");
+
+    qDebug() << str;
+
+    //QStringList environment = QProcess::systemEnvironment();
+
+
 
     //for (int i = 0; i < 1; i ++)
     AppRegistry::instance().settingsManager().save();
 
     ConnectionsDialog dialog(&AppRegistry::instance().settingsManager());
-    dialog.show();
+    dialog.exec();
 
-    ConnectionRecordPtr record1(new ConnectionRecord);
-    ConnectionRecordPtr record2(new ConnectionRecord);
-    ConnectionRecordPtr record1a = record1;
-    ConnectionRecordPtr record1b = record1a;
-    ConnectionRecordPtr record2a = record2;
+    AppRegistry::instance().settingsManager().save();
 
-    record1.data();
-
-    QList<ConnectionRecordPtr> list;
-    list.append(record1);
-    list.append(record2);
-
-    list.removeOne(record1a);
-    list.removeOne(record1b);
-    list.removeOne(record2a);
-
-    QHash<ConnectionRecordPtr, QString> hash;
-    hash.insert(record1, "one");
-    hash.insert(record2, "two");
-
-    hash.remove(record1a);
-    hash.remove(record1b);
-    hash.remove(record2a);
-
-
-//    ConnectionRecord man1;
-//    ConnectionRecord man2;
-//    ConnectionRecord man1a = man1;
-//    ConnectionRecord man1b = man1a;
-
-//    bool z1 = man1 == man2;
-//    bool z2 = man2 == man1a;
-//    bool z3 = man1b == man1a;
-
-//    QList<ConnectionRecord> list;
-//    list.append(man1);
-//    list.append(man2);
-
-//    list.removeOne(man1);
-//    list.removeOne(man1a);
-//    list.removeOne(man2);
-
-//    QHash<ConnectionRecord, QString> hash;
-//    hash.insert(man1, "hello");
-//    hash.insert(man2, "aga");
-
-//    hash.remove(man1a);
-//    hash.remove(man1);
-//    hash.remove(man2);
-
-
+    app.exit();
+    return 0;
 
 //    MainWindow w;
 //    w.show();

5fadd4300fc9635d6bce7461aec0d1be4aa01a4c
schetnikovich <tschetnikovich@gmail.com>
2012-12-16 19:10:08 +0300

    close tab icons

--- src/gui/widgets/workarea/WorkAreaTabBar.cpp
+++ src/gui/widgets/workarea/WorkAreaTabBar.cpp
@@ -5,6 +5,18 @@ using namespace Robomongo;
 
 WorkAreaTabBar::WorkAreaTabBar(QWidget *parent) : QTabBar(parent)
 {
+    setStyleSheet("QTabBar::close-button { "
+                      "image: url(:/robomongo/icons/close_16x16.png);"
+                      "width: 13px;"
+                      "height: 13px;"
+                  "}"
+                  "QTabBar::close-button:hover { "
+                                        "image: url(:/robomongo/icons/close_hover_16x16.png);"
+                                        "width: 15px;"
+                                        "height: 15px;"
+                                    "}"
+    );
+
 }
 
 void WorkAreaTabBar::mouseReleaseEvent(QMouseEvent * event)

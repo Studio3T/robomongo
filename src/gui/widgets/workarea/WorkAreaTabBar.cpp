d2e6acdac4f0471907fa6e1035d569f4c57841c0
schetnikovich <tschetnikovich@gmail.com>
2013-01-05 02:44:04 +0300

    tab bottom color change

--- src/gui/widgets/workarea/WorkAreaTabBar.cpp
+++ src/gui/widgets/workarea/WorkAreaTabBar.cpp
@@ -141,7 +141,7 @@ QString WorkAreaTabBar::buildStyleSheet()
                                         "stop: 0 #F0F0F0, stop: 0.4 #DEDEDE,"
                                         "stop: 0.5 #E6E6E6, stop: 1.0 #E1E1E1);"
             "border: 1px solid #C4C4C3;"
-            "border-bottom-color: #E1E1E1;" // #C2C7CB same as the pane color
+            "border-bottom-color: #B8B7B6;" // #C2C7CB same as the pane color
             "border-top-left-radius: 6px;"
             "border-top-right-radius: 6px;"
             "min-width: 8ex;"

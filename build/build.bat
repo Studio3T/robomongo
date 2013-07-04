d006069d870919d0f24add3ee09f2859ca897bef
Alexandr <topilski@github.com>
2013-07-04 10:26:44 +0300

    fix bat files for building application

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,20 @@
-cmake ../ -G "Visual Studio 10"
-cmake --build . --target robomongo --config Release
-cmake --build . --target install
+@echo off 
+setlocal enableextensions enabledelayedexpansion
+rem -----------------------------------
+rem - Configuration
+rem -----------------------------------
+rem arch_bit is a first arg
+mkdir build
+cd build
+set arch_bit=%1
+if %1. ==. (
+set arch_bit=32
+)
+rem CMAKE_INSTALL_PREFIX
+if %arch_bit% == 32 (
+	cmake ../../ -G "Visual Studio 10" -DFORCE_BUILD=1
+) else (
+	cmake ../../ -G "Visual Studio 10 Win64" -DFORCE_BUILD=1
+)
+rem cmake --build . --target robomongo --config Release
+cmake --build . --target install --config Release

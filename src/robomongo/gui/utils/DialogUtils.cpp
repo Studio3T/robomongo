af8635939b64d22ce6c4bc006f65004a2377713b
topilski <topilski@github.com>
2013-07-18 11:06:03 +0300

    Drop Index confirmation required #149

---
+++ src/robomongo/gui/utils/DialogUtils.cpp
@@ -0,0 +1,23 @@
+#include "robomongo/gui/utils/DialogUtils.h"
+
+namespace Robomongo
+{
+    namespace utils
+    {
+        namespace
+        {
+            const QString titleTemaple = QString("%1 %2");
+            const QString textTemaple = QString("%1 <b>%3</b> %2?");
+        }
+
+        int questionDialog(QWidget *parent, const QString &actionText, const QString &itemText, const QString& valueText)
+        {
+            return questionDialog(parent,actionText,itemText,textTemaple,valueText);
+        }
+
+        int questionDialog(QWidget *parent, const QString &actionText, const QString &itemText, const QString &templateText, const QString &valueText)
+        {
+            return QMessageBox::question(parent,titleTemaple.arg(actionText).arg(itemText),templateText.arg(actionText).arg(itemText.toLower()).arg(valueText), QMessageBox::Yes, QMessageBox::No, QMessageBox::NoButton);
+        }
+    }
+}

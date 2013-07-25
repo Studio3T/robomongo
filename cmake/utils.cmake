f7b05a55a4b26ff3554a68ee72564ebd84f33d4d
topilski <topilski@github.com>
2013-07-25 13:33:51 +0300

    Generate win.rc file, add template rc for other platforms

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,35 +1,29 @@
 macro(VersionConf prjName from_file to_file)
-set(COMPANYNAME "\"${PROJECT_COPYRIGHT}\"")
-string(REPLACE "." ";" versionList ${PROJECT_VERSION})
+SET(COMPANYNAME "\"${PROJECT_COMPANYNAME}\"")
 
-list(GET versionList 0 MAJOR_VER)
-list(GET versionList 1 MINOR_VER1)
-list(GET versionList 2 MINOR_VER2)
-list(GET versionList 3 MINOR_VER3)
-
-set(filecontent "
-	set(MAJOR_VER	${MAJOR_VER} ) 	
-	set(MINOR_VER1	${MINOR_VER1})
-	set(MINOR_VER2	${MINOR_VER2})
-	set(MINOR_VER3	${MINOR_VER3})
-	set(PRODUCTNAME ${prjName})
-	set(COMPANYNAME ${COMPANYNAME})
-	set(PRODUCTDOMAIN ${PROJECT_DOMAIN})
-	configure_file(${from_file} ${to_file})
+SET(filecontent "
+    SET(ICON_FILE ${ICON_FILE})
+	SET(MAJOR_VER	${MAJOR} ) 	
+	SET(MINOR_VER1	${MINOR})
+	SET(MINOR_VER2	${PATCH})
+	SET(PRODUCTNAME ${prjName})
+	SET(COMPANYNAME ${COMPANYNAME})
+	SET(PRODUCTDOMAIN ${PROJECT_DOMAIN})
+    SET(SHORTPRODUCTNAME ${prjName})
+	configure_file(${from_file} ${to_file})   
 ")
-
 file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/versionConfFile.cmake" "${filecontent}")
 
-add_custom_target(VersionConfSvn ALL DEPENDS ${to_file})
+add_custom_target(VersionConf ALL DEPENDS ${to_file})
 add_custom_command(OUTPUT ${to_file}
     COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR} -P ${CMAKE_CURRENT_BINARY_DIR}/versionConfFile.cmake)
 
 set_source_files_properties(
 	${to_file}
     PROPERTIES GENERATED TRUE
-    HEADER_FILE_ONLY TRUE
+    #HEADER_FILE_ONLY TRUE
 )
 
-add_dependencies(${prjName} VersionConfSvn)
+add_dependencies(${prjName} VersionConf)
 
 endmacro(VersionConf)

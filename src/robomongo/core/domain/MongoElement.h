3fab28028d6f2f75fb828ea03b386bb093e89db5
topilski <topilski@github.com>
2013-08-06 11:42:12 +0300

    Remove tab symbols from all source files. #197

--- src/robomongo/core/domain/MongoElement.h
+++ src/robomongo/core/domain/MongoElement.h
@@ -2,80 +2,83 @@
 #include <QString>
 #include <mongo/bson/bsonobj.h>
 #include "robomongo/core/Core.h"
+
 namespace Robomongo
 {
-	class Concatenator;
+    class Concatenator;
 
-	/*
-	** Wrapper around BSONElement
-	*/
-	class MongoElement 
-	{
-	public:
-		/*
-		** Create instance of MongoElement from BSONElement
-		*/
+    /*
+    ** Wrapper around BSONElement
+    */
+    class MongoElement 
+    {
+    public:
+
+        /*
+        ** Create instance of MongoElement from BSONElement
+        */
         MongoElement(mongo::BSONElement bsonElement);
 
-		/*
-		** String value. Only for Simple types
-		*/
-		QString stringValue();
+        /*
+        ** String value. Only for Simple types
+        */
+        QString stringValue();
 
-		/*
-		** Get field name
-		*/
-		QString fieldName();
+        /*
+        ** Get field name
+        */
+        QString fieldName();
 
-		/*
-		** Check if this element is of simple type
-		*/
-		bool isSimpleType() const { return _bsonElement.isSimpleType(); }
+        /*
+        ** Check if this element is of simple type
+        */
+        bool isSimpleType() const { return _bsonElement.isSimpleType(); }
 
         /**
-         * @brief Check if this element is of UUID type
-         */
+        * @brief Check if this element is of UUID type
+        */
         bool isUuidType() const {
-            if (_bsonElement.type() != mongo::BinData)
-                return false;
+        if (_bsonElement.type() != mongo::BinData)
+        return false;
 
-            mongo::BinDataType binType = _bsonElement.binDataType();
-            return (binType == mongo::newUUID || binType == mongo::bdtUUID);
+        mongo::BinDataType binType = _bsonElement.binDataType();
+        return (binType == mongo::newUUID || binType == mongo::bdtUUID);
         }
 
-		/*
-		** Check if this element is a BSON Object
-		*/
-		bool isDocument() const { return _bsonElement.isABSONObj(); }
+        /*
+        ** Check if this element is a BSON Object
+        */
+        bool isDocument() const { return _bsonElement.isABSONObj(); }
 
-		/*
-		** Check if this element is an BSON array
-		*/
+        /*
+        ** Check if this element is an BSON array
+        */
         bool isArray() const { return _bsonElement.isABSONObj() && _bsonElement.type() == mongo::Array; }
 
-		/*
-		** Check if this element is a BSON String
-		*/
+        /*
+        ** Check if this element is a BSON String
+        */
         bool isString() const { return _bsonElement.type() == mongo::String; }
 
-		/*
-		** Return bson type
-		*/
+        /*
+        ** Return bson type
+        */
         mongo::BSONType type() const { return _bsonElement.type(); }
 
-		/*
-		** Return MongoDocument of this element (you should check that this IS document before)
-		*/
+        /*
+        ** Return MongoDocument of this element (you should check that this IS document before)
+        */
         MongoDocumentPtr asDocument();
 
-		/*
-		** Build Json string that represent this element.
-		*/
+        /*
+        ** Build Json string that represent this element.
+        */
         void buildJsonString(Concatenator &con);
 
         mongo::BSONElement bsonElement() const { return _bsonElement; }
 
     private:
+
         /*
         ** Native BSONElement this MongoElement represents
         */
@@ -85,5 +88,5 @@ namespace Robomongo
         ** String value if this element
         */
         QString _stringValue;
-	};
+    };
 }

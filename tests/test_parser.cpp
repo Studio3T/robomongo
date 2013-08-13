c166f4ad7a2d74160b905604b8636499cdc11944
topilski <topilski@github.com>
2013-08-13 14:16:10 +0300

    Added first test case, using gtest solution

---
+++ tests/test_parser.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include <mongo/bson/bsonobjbuilder.h>
+#include "robomongo/core/utils/BsonUtils.h"
+using namespace Robomongo;
+TEST(JsonString, ResetsToZero) {
+    mongo::BSONObj obj;
+    std::string str = BsonUtils::jsonString(obj, mongo::TenGen, 1, DefaultEncoding);
+    EXPECT_EQ("{}", str);
+}

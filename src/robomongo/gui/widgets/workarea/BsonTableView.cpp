b675764379149ca3e9121c615f0428f7e9654121
topilski <topilski@github.com>
2013-09-03 13:52:08 +0300

    Align text in table's header to the left (when in Table Mode) #270

--- src/robomongo/gui/widgets/workarea/BsonTableView.cpp
+++ src/robomongo/gui/widgets/workarea/BsonTableView.cpp
@@ -1,9 +1,13 @@
 #include "robomongo/gui/widgets/workarea/BsonTableView.h"
 
+#include <QHeaderView>
+
 namespace Robomongo
 {
     BsonTableView::BsonTableView(MongoShell *shell, QWidget *parent) 
         :BaseClass(parent)
     {
+        verticalHeader()->setDefaultAlignment(Qt::AlignLeft);
+        horizontalHeader()->setDefaultAlignment(Qt::AlignLeft);
     }
 }

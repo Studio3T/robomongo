d4d624302528e071d3baf69770faf6ddc2f61303
topilski <topilski@github.com>
2013-09-06 16:16:32 +0300

    Add background to tableview

--- src/robomongo/gui/widgets/workarea/BsonTableView.cpp
+++ src/robomongo/gui/widgets/workarea/BsonTableView.cpp
@@ -34,7 +34,7 @@ namespace Robomongo
         verticalHeader()->setDefaultAlignment(Qt::AlignLeft);
         horizontalHeader()->setDefaultAlignment(Qt::AlignLeft);
         //horizontalHeader()->setFixedHeight(25);   // commented because we shouldn't depend on heights in pixels - it may vary between platforms
-        setStyleSheet("QTableView { border-left: 1px solid #c7c5c4; border-top: 1px solid #c7c5c4; }");
+        setStyleSheet("QTableView { border-left: 1px solid #c7c5c4; border-top: 1px solid #c7c5c4; gridline-color: #edebea;}");
         //setShowGrid(false);
         setSelectionMode(QAbstractItemView::SingleSelection);
 

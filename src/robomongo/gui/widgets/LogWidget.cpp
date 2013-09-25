d153271bc413274c6ace948c498ada1646564221
topilski <topilski@github.com>
2013-09-25 20:51:25 +0300

    Added "Clear All" action in LogWidget context menu

--- src/robomongo/gui/widgets/LogWidget.cpp
+++ src/robomongo/gui/widgets/LogWidget.cpp
@@ -2,6 +2,8 @@
 
 #include <QHBoxLayout>
 #include <QScrollBar>
+#include <QMenu>
+#include <QAction>
 #include <QPlainTextEdit>
 #include "robomongo/core/utils/QtUtils.h"
 
@@ -10,14 +12,26 @@ namespace Robomongo
     LogWidget::LogWidget(QWidget* parent) 
         : BaseClass(parent), _logTextEdit(new QPlainTextEdit(this))
     {
-        _logTextEdit->setPlainText(PROJECT_NAME " " PROJECT_VERSION " is ready.");
-        //_logTextEdit->setMarginWidth(1, 3); // to hide left gray column
+        _logTextEdit->setPlainText(PROJECT_NAME" "PROJECT_VERSION" is ready.");
         _logTextEdit->setReadOnly(true);
+        _logTextEdit->setContextMenuPolicy(Qt::CustomContextMenu);
+        VERIFY(connect(_logTextEdit,SIGNAL(customContextMenuRequested(const QPoint&)),this,SLOT(showContextMenu(const QPoint &))));
         QHBoxLayout *hlayout = new QHBoxLayout;
-        hlayout->setMargin(0);
+        hlayout->setContentsMargins(0,0,0,0);
         hlayout->addWidget(_logTextEdit);
-
+        _clear = new QAction("Clear All", this);
+        VERIFY(connect(_clear, SIGNAL(triggered()),_logTextEdit, SLOT(clear())));
         setLayout(hlayout);      
+    }
+
+    void LogWidget::showContextMenu(const QPoint &pt)
+    {
+        QMenu *menu = _logTextEdit->createStandardContextMenu();
+        menu->addAction(_clear);
+        _clear->setEnabled(!_logTextEdit->toPlainText().isEmpty());
+
+        menu->exec(_logTextEdit->mapToGlobal(pt));
+        delete menu;
     }
 
     void LogWidget::addMessage(const QString &message, mongo::LogLevel level)

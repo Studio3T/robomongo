df99ba2e33c26a751ad9b5c70bbb88800c7b0f37
topilski <topilski@github.com>
2013-10-28 15:36:43 +0300

    Fixup build cpack script

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ createPackage() {
     fi
     mkdir "$dir_path"
     cd "$dir_path"
-    cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCPACK_GENERATOR="$cpack_generator" -DOPENSSL_USE_STATIC=$3
+    cmake ../../ -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RELEASE -DCPACK_GENERATOR="$cpack_generator"
     make install
     cpack -G "$cpack_generator"
     if [ "$cpack_generator" = 'DEB' ]; then
@@ -21,11 +21,11 @@ createPackage() {
 unamestr=`uname`
 
 if [ "$unamestr" = 'Linux' ]; then
-    createPackage build_deb DEB 1
-    createPackage build_rpm RPM 1
-    createPackage build_tar TGZ 1
+    createPackage build_deb DEB
+    createPackage build_rpm RPM
+    createPackage build_tar TGZ
 elif [ "$unamestr" = 'Darwin' ]; then
-    createPackage build_dmg DragNDrop 0
-    createPackage build_zip ZIP 0
+    createPackage build_dmg DragNDrop
+    createPackage build_zip ZIP
 fi
 

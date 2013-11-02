b517dc9043d39575b393b6dc341c96eadf43d755
Vladimir Belozyorov <vladimir.belozyorov@gmail.com>
2013-11-02 22:46:24 +0400

    ts_compile mapping sorted

--- src/robomongo/lang/ts_compile.sh
+++ src/robomongo/lang/ts_compile.sh
@@ -4,31 +4,43 @@
 # So...
 
 declare -A mapping
-mapping[MainWindow]=Robomongo
+# core/domain
+mapping[Notifier]=Robomongo
+# core/mongodb
+mapping[MongoWorker]=Robomongo
+# core/settings
 mapping[SettingsManager]=Robomongo
-mapping[EditIndexDialog]=Robomongo
-mapping[FindFrame]=Robomongo
+# gui/
+mapping[MainWindow]=Robomongo
+# gui/dialogs
 mapping[AboutDialog]=Robomongo
-mapping[ConnectionsDialog]=Robomongo
+mapping[ConnectionAdvancedTab]=Robomongo
 mapping[ConnectionAuthTab]=Robomongo
 mapping[ConnectionBasicTab]=Robomongo
-mapping[ConnectionAdvancedTab]=Robomongo
 mapping[ConnectionDiagnosticDialog]=Robomongo
 mapping[ConnectionDialog]=Robomongo
 mapping[ConnectionSslTab]=Robomongo
 mapping[ConnectionsDialog]=Robomongo
-mapping[CopyCollection]=Robomongo
+mapping[CopyCollectionDialog]=Robomongo
 mapping[CreateDatabaseDialog]=Robomongo
 mapping[CreateUserDialog]=Robomongo
 mapping[DocumentTextEditor]=Robomongo
 mapping[FunctionTextEditor]=Robomongo
+mapping[PreferencesDialog]=Robomongo
 mapping[SshTunelTab]=Robomongo
-mapping[ExplorerServerTreeItem]=Robomongo
-mapping[ExplorerDatabaseTreeItem]=Robomongo
+# gui/editors
+mapping[FindFrame]=Robomongo
+# gui/widgets
+mapping[LogWidget]=Robomongo
+# gui/widgets/explorer
+mapping[EditIndexDialog]=Robomongo
 mapping[ExplorerCollectionTreeItem]=Robomongo
 mapping[ExplorerDatabaseCategoryTreeItem]=Robomongo
+mapping[ExplorerDatabaseTreeItem]=Robomongo
 mapping[ExplorerFunctionTreeItem]=Robomongo
+mapping[ExplorerServerTreeItem]=Robomongo
 mapping[ExplorerUserTreeItem]=Robomongo
+# gui/widgets/workarea
 mapping[BsonTreeModel]=Robomongo
 mapping[BsonTreeView]=Robomongo
 mapping[CollectionStatsTreeWidget]=Robomongo
@@ -39,11 +51,8 @@ mapping[QueryWidget]=Robomongo
 mapping[ScriptWidget]=Robomongo
 mapping[WorkAreaTabBar]=Robomongo
 mapping[WorkAreaTabWidget]=Robomongo
-mapping[LogWidget]=Robomongo
-mapping[FindFrame]=Robomongo
-mapping[Notifier]=Robomongo
 
-for file in `find ./ -name "*.raw.ts"`
+for file in `find ./ -name "robomongo_*.raw.ts"`
 do
     right_name=`echo "$file" | sed 's/.raw//g'`
     cp $file $right_name

fbddb83cc70a2b2fee609e7ed2d134003e3de309
Stephen Steneker <stennie@cpan.org>
2014-08-07 22:03:35 +1000

    Fix #622: Type of Double changed to Int32 on save document

--- src/robomongo/core/utils/BsonUtils.cpp
+++ src/robomongo/core/utils/BsonUtils.cpp
@@ -114,13 +114,15 @@ namespace Robomongo
                 s << "NumberLong(" << elem._numberLong() << ")";
                 break;
             case NumberInt:
+				s << elem.number();
+				break;
             case NumberDouble:
                 {
                     int sign=0;
                     if ( elem.number() >= -numeric_limits< double >::max() &&
                             elem.number() <= numeric_limits< double >::max() ) {
                         s.precision( 16 );
-                        s << elem.number();
+                        s << fixed << elem._numberDouble();
                     }
                     else if ( mongo::isNaN(elem.number()) ) {
                         s << "NaN";
@@ -130,7 +132,7 @@ namespace Robomongo
                     }
                     else {
                         StringBuilder ss;
-                        ss << "Number " << elem.number() << " cannot be represented in JSON";
+                        ss << "NumberDouble " << elem.number() << " cannot be represented in JSON";
                         string message = ss.str();
                         //massert( 10311 ,  message.c_str(), false );
                     }

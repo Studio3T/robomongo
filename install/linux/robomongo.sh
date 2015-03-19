7f54a7ec2cdc4d81a1f8b76a0e5f5820928c3d96
Stephen Steneker <stennie@cpan.org>
2015-03-19 21:00:58 +1100

    Fix #804: Some keys not working in 0.8.5 (Windows remote to Ubuntu running xrdp)

--- install/linux/robomongo.sh
+++ install/linux/robomongo.sh
@@ -13,5 +13,6 @@ ROBO_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 export LD_LIBRARY_PATH="$ROBO_DIR/../lib:$LD_LIBRARY_PATH"
 export QT_PLUGIN_PATH="$ROBO_DIR/../lib:$QT_PLUGIN_PATH"
 export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
+export XKB_DEFAULT_RULES=base
 
 "$ROBO_DIR/robomongo"

e87ff22264d37ff400cb465d05d811ddabfdc853
Alex Bevilacqua <alex@alexbevi.com>
2015-09-11 09:32:06 -0400

    BASE: remove deleted files from third-party/mongodb

--- src/third-party/mongodb/jstests/ssl/replset1.js
+++
@@ -1,5 +0,0 @@
-ssl_options = {sslOnNormalPorts : "",
-               sslPEMKeyFile : "jstests/libs/server.pem",
-               sslCAFile: "jstests/libs/ca.pem"};
-
-load("jstests/replsets/replset1.js");

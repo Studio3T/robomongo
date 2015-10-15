a939556832896c424549a9c5e43c3bc3e2359404
Alex Bevilacqua <alex@alexbevi.com>
2015-10-14 21:32:36 -0400

    update logger references

--- src/robomongo/core/utils/Logger.h
+++ src/robomongo/core/utils/Logger.h
@@ -3,22 +3,28 @@
 #include <QObject>
 #include <QString>
 #include <string>
+
+#ifndef MONGO_UTIL_LOG_H_
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+#include <mongo/logger/log_severity.h>
 #include <mongo/util/log.h>
+#endif
+
 #include "robomongo/core/utils/SingletonPattern.hpp"
 
 namespace Robomongo
-{  
+{
     class Logger
         : public QObject, public Patterns::LazySingleton<Logger>
     {
         friend class Patterns::LazySingleton<Logger>;
         Q_OBJECT
     public:
-        void print(const char *mess, mongo::LogLevel level, bool notify);
-        void print(const std::string &mess, mongo::LogLevel level, bool notify);
-        void print(const QString &mess, mongo::LogLevel level, bool notify);        
+        void print(const char *mess, ::mongo::logger::LogSeverity level, bool notify);
+        void print(const std::string &mess, ::mongo::logger::LogSeverity level, bool notify);
+        void print(const QString &mess, ::mongo::logger::LogSeverity level, bool notify);
     Q_SIGNALS:
-        void printed(const QString &mess, mongo::LogLevel level);
+        void printed(const QString &mess, ::mongo::logger::LogSeverity level);
 
     private:
         Logger();
@@ -26,7 +32,7 @@ namespace Robomongo
     };
 
     template<typename T>
-    inline void LOG_MSG(const T &mess, mongo::LogLevel level, bool notify = true)
+    inline void LOG_MSG(const T &mess, ::mongo::logger::LogSeverity level, bool notify = true)
     {
         return Logger::instance().print(mess, level, notify);
     }
--- src/robomongo/gui/widgets/LogWidget.cpp
+++ src/robomongo/gui/widgets/LogWidget.cpp
@@ -10,7 +10,7 @@
 
 namespace Robomongo
 {
-    LogWidget::LogWidget(QWidget* parent) 
+    LogWidget::LogWidget(QWidget* parent)
         : BaseClass(parent), _logTextEdit(new QTextEdit(this))
     {
         _logTextEdit->setReadOnly(true);
@@ -21,7 +21,7 @@ namespace Robomongo
         hlayout->addWidget(_logTextEdit);
         _clear = new QAction("Clear All", this);
         VERIFY(connect(_clear, SIGNAL(triggered()),_logTextEdit, SLOT(clear())));
-        setLayout(hlayout);      
+        setLayout(hlayout);
     }
 
     void LogWidget::showContextMenu(const QPoint &pt)
@@ -34,12 +34,12 @@ namespace Robomongo
         delete menu;
     }
 
-    void LogWidget::addMessage(const QString &message, mongo::LogLevel level)
+    void LogWidget::addMessage(const QString &message, ::mongo::logger::LogSeverity level)
     {
         QTime time = QTime::currentTime();
         QDate date = QDate::currentDate();
         //_logTextEdit->appendHtml(QString(level == mongo::LL_ERROR ? "<font color=red>%1 %2</font>" : "<font color=black>%1 %2</font>").arg(time.toString("h:mm:ss AP:")).arg(message.toHtmlEscaped()));
-        _logTextEdit->setTextColor(level == mongo::LL_ERROR ? QColor(Qt::red):QColor(Qt::black));
+        _logTextEdit->setTextColor(level == ::mongo::logger::LogSeverity::Error() ? QColor(Qt::red):QColor(Qt::black));
         _logTextEdit->append(date.toString("yyyy-MM-dd ") + time.toString("hh:mm:ss: ") + message);
         QScrollBar *sb = _logTextEdit->verticalScrollBar();
         sb->setValue(sb->maximum());

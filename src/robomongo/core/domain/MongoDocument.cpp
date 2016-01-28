6d06a3c83c7611499920611f00a84f095a2b541b
schetnikovich <tschetnikovich@gmail.com>
2016-01-28 19:50:25 +0300

    Cleanup of unused code

--- src/robomongo/core/domain/MongoDocument.cpp
+++ src/robomongo/core/domain/MongoDocument.cpp
@@ -44,46 +44,4 @@ namespace Robomongo
 
         return list;
     }
-
-    /*
-    ** Convert to json string
-    */
-    void MongoDocument::buildJsonString(std::string &con)
-    {
-        BsonUtils::buildJsonString(_bsonObj, con,
-            AppRegistry::instance().settingsManager()->uuidEncoding(),
-            AppRegistry::instance().settingsManager()->timeZone());
-    }
-
-    /*
-    ** Build JsonString from list of documents
-    */
-    std::string MongoDocument::buildJsonString(const QList<MongoDocumentPtr> &documents)
-    {
-        mongo::StringBuilder sb;
-
-        int position = 0;
-        for (QList<MongoDocumentPtr>::const_iterator it = documents.begin(); it != documents.end(); ++it) {
-            if (position == 0)
-                sb << "/* 0 */\n";
-            else 
-                sb << "\n\n/* " << position << " */\n";
-
-            std::string jsonString = (*it)->bsonObj().jsonString(mongo::TenGen, 1);
-
-            sb << jsonString;
-            position++;
-        }
-
-        std::string final = sb.str();
-        return final;
-    }
-
-    std::string MongoDocument::buildJsonString(const MongoDocumentPtr &doc)
-    {
-        //qt4 QTextCodec::setCodecForCStrings(codec);
-        std::string jsonString = doc->bsonObj().jsonString(mongo::TenGen, 1);
-        return jsonString;
-    }
-
 }

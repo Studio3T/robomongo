5162291f9d06e6ad2cfb74c95913a7f4e3137d25
Gokhan Simsek <g.simsek@paralect.com>
2017-05-10 17:55:21 +0300

    Adding "Beta" string into main window title

--- src/robomongo/gui/MainWindow.cpp
+++ src/robomongo/gui/MainWindow.cpp
@@ -620,7 +620,7 @@ namespace Robomongo
 
         createTabs();
         createStatusBar();
-        setWindowTitle("Robomongo " + QString(PROJECT_VERSION_SHORT));
+        setWindowTitle("Robomongo " + QString(PROJECT_VERSION_SHORT) + " - Beta");
         setWindowIcon(GuiRegistry::instance().mainWindowIcon());
 
         QTimer::singleShot(0, this, SLOT(manageConnections()));       

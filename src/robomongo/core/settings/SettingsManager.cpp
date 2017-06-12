2d021c80d3e2a7a87c2cf0e87b076b1ad13d2fab
Gokhan Simsek <g.simsek@paralect.com>
2017-06-12 09:40:22 +0300

    Adding support for import connections from version 1.1.0-Beta

--- src/robomongo/core/settings/SettingsManager.cpp
+++ src/robomongo/core/settings/SettingsManager.cpp
@@ -65,11 +65,14 @@ namespace Robomongo
     const auto CONFIG_FILE_1_0_RC1 = QString("%1/.config/robomongo/1.0/robomongo.json").arg(QDir::homePath());
     // Change of path due to re-branding to 3T
     const auto CONFIG_FILE_1_0_0 = QString("%1/.3T/robomongo/1.0.0/robomongo.json").arg(QDir::homePath());
+    const auto CONFIG_FILE_1_1_0_BETA = QString("%1/.3T/robomongo/1.1.0-Beta/robomongo.json").arg(QDir::homePath());
 
     // Important Note: In order to import connections from the latest version found, config. file path of an 
-    //                 old version must be defined and placed into the vector initializer list below in order.
+    //                 old version must be defined (i.e. CONFIG_FILE_1_0_0 seen above) and placed into the 
+    //                 vector initializer list below in order.
     std::vector<QString> const SettingsManager::_configFilesOfOldVersions
     {
+        CONFIG_FILE_1_1_0_BETA,
         CONFIG_FILE_1_0_0,
         CONFIG_FILE_1_0_RC1,
         CONFIG_FILE_0_9,

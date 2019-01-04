071bec0924a0c7be5f32a8a68f12d527200b83be
Gokhan Simsek <g.simsek@paralect.com>
2019-01-04 09:44:47 +0300

    First working version with dummy unit tests

---
+++ src/robomongo/utils/string_operations_test.cpp
@@ -0,0 +1,31 @@
+
+#include "gtest/gtest.h"
+#include "string_operations.h"
+
+#include <string>
+
+/* Example Test:
+ *
+ * TEST( [Test_Case_Name], [Test_Name] )
+ * TEST( [Test_Case_Name], [UnitOfWorkName_ScenarioUnderTest_ExpectedBehavior] )
+ * TEST( StringParserTests, NumberLeftOf_StringWithoutNumber_ReturnsFalse) {
+    // ...
+   }
+*/
+
+// (test_case_name, test_name) 
+TEST(string_operations_basic_tests, captilizeFirstChar)
+{
+    // EXPECT_EQ("Abcc", Robomongo::captilizeFirstChar("abc")); // Simulating failing test
+    // EXPECT_EQ("Abc", Robomongo::captilizeFirstChar("abc")); // Simulating passing test
+}
+
+TEST(string_operations_adv_tests, Test1)
+{
+    EXPECT_EQ(0, 0);
+}
+
+TEST(string_operations_adv_tests, Test2)
+{
+    EXPECT_EQ(0, 0);
+}

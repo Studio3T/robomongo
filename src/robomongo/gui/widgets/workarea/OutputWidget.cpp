97af0e28fc0aa0f02f93ba526e15221a19cfe771
Gokhan Simsek <g.simsek@paralect.com>
2019-02-26 11:22:47 +0300

    #1591 #1403: Tabbed output feature: Some style changes for all platforms

--- src/robomongo/gui/widgets/workarea/OutputWidget.cpp
+++ src/robomongo/gui/widgets/workarea/OutputWidget.cpp
@@ -246,12 +246,9 @@ namespace Robomongo
         QString aga3 = background.name();
 
         QString styles = QString(
+#ifdef __APPLE__      
             "QTabBar::close-button { "
-                #ifdef __APPLE__           
-                    "image: url(:/robomongo/icons/close_2_Mac_16x16.png);"
-                #else       
-                    "image: url(:/robomongo/icons/close_2_16x16.png);"
-                #endif      
+                "image: url(:/robomongo/icons/close_2_Mac_16x16.png);"
                 "width: 10px;"
                 "height: 10px;"
                 "}"
@@ -261,11 +258,9 @@ namespace Robomongo
                 "height: 15px;"
             "}"
             "QTabBar::tab {"
-                #ifdef __APPLE__
-                    "height: 18px;"    
-                #else
-                    "width: 58px;"
-                #endif
+                "height: 18px;"    
+                "margin-right: 1px;"   
+                "margin-top: 1px;"                    
                 "background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
                 "stop: 0 #F0F0F0, stop: 0.4 #DEDEDE,"
                 "stop: 0.5 #E6E6E6, stop: 1.0 #E1E1E1);"
@@ -273,38 +268,58 @@ namespace Robomongo
                 "border-bottom-color: #B8B7B6;" // #C2C7CB same as the pane color
                 "border-top-left-radius: 3px;"
                 "border-top-right-radius: 3px;"
-                #ifdef __APPLE__
-                    "padding: 4px 0px 5px 0px;" // top r b l
-                #else
-                    "padding: 4px 0px 4px 4px;" // top r b l
-                #endif
-                #ifndef __APPLE__
-                    "max-width: 200px;"
-                    "margin: 0px;"
-                    "margin-left: 1px;"
-                    "margin-right: -3px;"  
-                        // it should be -(tab:first:margin-left + tab:last:margin-left) 
-                        // to fix incorrect text elidement                
-                #endif
+                "padding: 4px 0px 5px 0px;" // top r b l
             "}"
             "QTabBar::tab:selected, QTabBar::tab:hover {"
                 "/* background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0,"
-                "stop: 0 %1, stop: 0.3 %2,"    //#fafafa, #f4f4f4
-                "stop: 0.6 %3, stop: 1.0 %4); */" //#e7e7e7, #fafafa
+                                "stop: 0 %1, stop: 0.3 %2,"    //#fafafa, #f4f4f4
+                                "stop: 0.6 %3, stop: 1.0 %4); */" //#e7e7e7, #fafafa
                 "background-color: white;"
             "}"
-
             "QTabBar::tab:selected {"
-                "margin-right: 1px;"
+                "border-color: #9B9B9B;" //
+                "border-bottom-color: %4;" //#fafafa
+            "}"
+#else            
+            "QTabBar::close-button { "
+                "image: url(:/robomongo/icons/close_2_16x16.png);"
+                "width: 10px;"
+                "height: 10px;"
+            "}"
+            "QTabBar::close-button:hover { "
+                  "image: url(:/robomongo/icons/close_hover_16x16.png);"
+                  "width: 15px;"
+                  "height: 15px;"
+            "}"
+            "QTabBar::tab {"
+                "background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
+                                            "stop: 0 #F0F0F0, stop: 0.4 #DEDEDE,"
+                                            "stop: 0.5 #E6E6E6, stop: 1.0 #E1E1E1);"
+                "border: 1px solid #C4C4C3;"
+                "border-bottom-color: #B8B7B6;" // #C2C7CB same as the pane color
+                "border-top-left-radius: 6px;"
+                "border-top-right-radius: 6px;"
+                "padding: 4px 4px 5px 8px;"
+                "max-width: 200px;"
+                "margin: 0px;"
+                "margin-left: 1px;"
+                "margin-right: -3px;"  // it should be -(tab:first:margin-left + tab:last:margin-left) to fix incorrect text elidement                
+            "}"
+            "QTabBar::tab:selected, QTabBar::tab:hover {"
+                "/* background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0,"
+                                "stop: 0 %1, stop: 0.3 %2,"    //#fafafa, #f4f4f4
+                                "stop: 0.6 %3, stop: 1.0 %4); */" //#e7e7e7, #fafafa            
+                "background-color: white;"
+            "}"
+            "QTabBar::tab:selected {"
                 "margin-top: 1px;"
                 "border-color: #9B9B9B;" //
                 "border-bottom-color: %4;" //#fafafa
             "}"
-
             "QTabBar::tab:!selected {"
-                "margin-right: 1px;"   
-                "margin-top: 1px;"
-            "}  "
+                "margin-top: 2px;" // make non-selected tabs look smaller
+            "}"
+#endif            
         ).arg(gradientZero.name(), gradientOne.name(), gradientTwo.name(), "#ffffff");
 
         QString aga = palette().window().color().name();

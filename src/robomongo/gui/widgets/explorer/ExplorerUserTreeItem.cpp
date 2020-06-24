f6ac15799304df3285725dc1f0ea78073b082b14
Gokhan Simsek <g.simsek@paralect.com>
2020-06-24 17:45:45 +0300

    #1728: Removing an obsolete function which caused crash when Users folder expanded and minor refactoring

--- src/robomongo/gui/widgets/explorer/ExplorerUserTreeItem.cpp
+++ src/robomongo/gui/widgets/explorer/ExplorerUserTreeItem.cpp
@@ -14,31 +14,16 @@
 #include "robomongo/core/settings/ConnectionSettings.h"
 #include "robomongo/core/utils/QtUtils.h"
 
-namespace
-{
-    const char* tooltipTemplate = 
-        "%s "
-        "<table>"
-        "<tr><td>ID:</td><td width=\"180\"><b>&nbsp;&nbsp;%s</b></td></tr>"
-        "</table>";
-
-    std::string buildToolTip(const Robomongo::MongoUser &user)
-    {
-        char buff[2048] = {0};
-        sprintf(buff, tooltipTemplate, user.name().c_str());
-        return buff;
-    }
-}
-
 namespace Robomongo
 {
-    ExplorerUserTreeItem::ExplorerUserTreeItem(QTreeWidgetItem *parent, MongoDatabase *const database, const MongoUser &user) :
-        BaseClass(parent), _user(user), _database(database)
+    ExplorerUserTreeItem::ExplorerUserTreeItem(
+        QTreeWidgetItem *parent, MongoDatabase *const database, const MongoUser &user) 
+        : BaseClass(parent), _user(user), _database(database)
     {
-        QAction *dropUser = new QAction("Drop User", this);
+        auto const dropUser { new QAction("Drop User", this) };
         VERIFY(connect(dropUser, SIGNAL(triggered()), SLOT(ui_dropUser())));
 
-        QAction *viewUser = new QAction("View User", this);
+        auto const viewUser { new QAction("View User", this) };
         VERIFY(connect(viewUser, SIGNAL(triggered()), SLOT(ui_viewUser())));
 
         BaseClass::_contextMenu->addAction(viewUser);
@@ -47,15 +32,16 @@ namespace Robomongo
         setText(0, QtUtils::toQString(_user.name()));
         setIcon(0, GuiRegistry::instance().userIcon());
         setExpanded(false);
-        //setChildIndicatorPolicy(QTreeWidgetItem::ShowIndicator);
-        setToolTip(0, QtUtils::toQString(buildToolTip(user)));
     }
 
     void ExplorerUserTreeItem::ui_dropUser()
     {
         // Ask user
-        int const answer = utils::questionDialog(treeWidget(), "Drop", "User", 
-                                                 QtUtils::toQString(_user.name()));
+        int const answer {
+            utils::questionDialog(
+                treeWidget(), "Drop", "User", QtUtils::toQString(_user.name())
+            )
+        };
 
         if (answer == QMessageBox::Yes)
             _database->dropUser(_user.name());
@@ -63,8 +49,10 @@ namespace Robomongo
 
     void ExplorerUserTreeItem::ui_viewUser()
     {
-        auto const& app = Robomongo::AppRegistry::instance().app();
-        app->openShell(_database, QString::fromStdString("db.getUser(\"" + _user.name() + "\")"), true,
-                        Robomongo::QtUtils::toQString(_database->name()), Robomongo::CursorPosition());
+        auto const& app { Robomongo::AppRegistry::instance().app() };
+        app->openShell(
+            _database, QString::fromStdString("db.getUser(\"" + _user.name() + "\")"), true,
+            QtUtils::toQString(_database->name()), CursorPosition()
+        );
     }
 }

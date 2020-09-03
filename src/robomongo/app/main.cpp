12e54ccda9e23400ac2c37519761e830e6f37c02
Gokhan Simsek <g.simsek@paralect.com>
2020-09-03 14:12:14 +0300

    #1757: Disabling IPv6 due to unexpected behaviors

--- src/robomongo/app/main.cpp
+++ src/robomongo/app/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[], char** envp)
     // Todo from mongo repo: This should use a TransportLayerManager or TransportLayerFactory
     auto serviceContext = mongo::getGlobalServiceContext();
     mongo::transport::TransportLayerASIO::Options opts;
-    opts.enableIPv6 = true;
+    opts.enableIPv6 = false; // Todo: When true, it breaks connection to localhost, github #1757
     opts.mode = mongo::transport::TransportLayerASIO::Options::kEgress;
     serviceContext->setTransportLayer(
         std::make_unique<mongo::transport::TransportLayerASIO>(opts, nullptr)

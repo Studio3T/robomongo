199a1864aaa68c4474e57f0d18e3a57a0525291b
Gökhan Şimşek <g.simsek@paralect.com>
2020-12-29 11:26:20 +0300

    Update WelcomeTab.cpp

--- src/robomongo/gui/widgets/workarea/WelcomeTab.cpp
+++ src/robomongo/gui/widgets/workarea/WelcomeTab.cpp
@@ -12,9 +12,8 @@ namespace Robomongo {
         QWidget(parent), _parent(parent)
     {
         auto webView = new QWebEngineView(this);
-        QUrl url{
-            "http://files.studio3t.com/rm-feed_3t_io/1.4.3/index.html"
         QUrl const URL {
+            "http://files.studio3t.com/rm-feed_3t_io/1.4.3/index.html"        
         };
         webView->setPage(new MyWebPage(this));
         webView->page()->setUrl(URL);

bdc9779df2c5e2ffd50aa6f957e1ae9c2d169659
Gokhan Simsek <g.simsek@paralect.com>
2021-01-04 17:57:10 +0300

    run-cppcheck.bat: Enable FILE or DIR argument passing

--- bin/run-cppcheck.bat
+++ bin/run-cppcheck.bat
@@ -2,16 +2,21 @@
 setlocal enableextensions enabledelayedexpansion
 
 set CPP_VER=c++17
-set DIR=..\src\robomongo\
+
+set FILE_OR_DIR=..\src\robomongo\
+set PARAM_1=%1
+if not "%PARAM_1%" == "" (
+  set FILE_OR_DIR=%PARAM_1%
+)
 
 rem Run analysis
 echo.
 echo ---------------------- Running cppcheck ----------------------
 echo.
-call cppcheck %DIR% --enable=all --std=%CPP_VER%
+call cppcheck %FILE_OR_DIR% --enable=all --std=%CPP_VER%
 echo.
 echo ---------------------- End of cppcheck ----------------------
 echo.
 echo ---------------------- Info ----------------------
 call cppcheck --version
-echo Command called: cppcheck %DIR% --enable=all --std=%CPP_VER%
+echo Command called: cppcheck %FILE_OR_DIR% --enable=all --std=%CPP_VER%
